c1d3752d3371e8107f9934a89cc04dc5013c1f7a
wxk <871165091@qq.com>
2026-03-31 19:34:02 +0800

    更新apk链接地址

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
     applicationId = "com.xyzw.webhelper"
     minSdk = 24
     targetSdk = 34
-    versionCode = 2
-    versionName = "1.0.1"
+    versionCode = 3
+    versionName = "1.0.3"
   }
 
   buildTypes {
